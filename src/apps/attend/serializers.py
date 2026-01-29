cc8665b5720388788fb23bf7e314e1b4022b4357
suwalkrizal <krijalsuwal67@gmail.com>
2026-01-29 13:38:26 +0545

    feat: Integrate Django REST Framework and define serializers for Room, Student, and Attendance models.

---
+++ src/apps/attend/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import(
+    'Teacher',
+    'Room',
+    'Student',
+    'Attendance',
+)
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+        
+class AttendanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attendance
+        fields = '__all__'
--- src/config/settings.py
+++ src/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.attend',
     'apps.management',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
